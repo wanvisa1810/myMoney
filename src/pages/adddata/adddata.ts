0a34a374e3c7a5328baa93b98a59c1000a44f9d6
Wanvisa Suwannin <wanvisa.nun@gmail.com>
2019-04-07 17:51:21 +0700

    ub add home

--- src/pages/adddata/adddata.ts
+++ src/pages/adddata/adddata.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { Toast } from '@ionic-native/toast/ngx';
 
 /**
  * Generated class for the AdddataPage page.
@@ -14,12 +16,57 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'adddata.html',
 })
 export class AdddataPage {
+  data = {date:"", type:"",description:"", amount:0 };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private sqlite: SQLite,
+    private toast: Toast) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AdddataPage');
   }
-
-}
+  saveData(){
+    this.sqlite.create({
+      name: 'ionicdb.db',
+      location: 'default'
+    })
+      .then(
+        (db: SQLiteObject) =>{
+          db.executeSql('INSERT INTO expense VALUES (NULL,?,?,?,?)',
+          [
+            this.data.date,
+            this.data.type,
+            this.data.description,
+            this.data.amount
+            ])
+            .then(
+              res=>{
+              console.log(res);
+              //แสดง popup (toast)
+              this.toast.show('Data saved','3000','center')
+              .subscribe(toast=>{
+                //this.navCtrl.popToRoot;
+                console.log(toast);
+              }
+              );
+            })
+            .catch(e=>{
+              console.log(e);
+              this.toast.show(e,'3000','center')
+              .subscribe(toast=>{
+                console.log(toast);
+              }
+            );
+        });
+      })
+        .catch(e=>{
+          console.log(e);
+          this.toast.show(e,'3000','center')
+          .subscribe(toast=>{
+            console.log(toast);
+          }
+          );
+          })
+        }
+      }
