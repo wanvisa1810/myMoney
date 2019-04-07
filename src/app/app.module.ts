5389e70eb9a457df8fcacbffd5d0aec38d5fbb06
Wanvisa Suwannin <wanvisa.nun@gmail.com>
2019-04-07 10:37:03 +0700

    ubload module,home.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AdddataPage } from '../pages/adddata/adddata';
+import { EditdataPage } from '../pages/editdata/editdata';
+import { SQLite } from '@ionic-native/sqlite/ngx';
+import { Toast } from '@ionic-native/toast/ngx';
+
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AdddataPage,
+    EditdataPage
   ],
   imports: [
     BrowserModule,
@@ -19,12 +27,15 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AdddataPage,
+    EditdataPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SQLite,Toast
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,99 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SQLite, SQLiteObject} from '@ionic-native/sqlite/ngx';
+import { AdddataPage } from '../adddata/adddata';
+import { EditdataPage } from '../editdata/editdata';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  expenses: any = [];
+  totalIncome = 0;
+  totalExpense = 0;
+  balance = 0;
+  constructor(public navCtrl: NavController, public sqlite : SQLite) {
   }
 
-}
+  //Load Page
+  ionViewDidLoad(){
+    this.getData();
+  }
+
+  //Back Page
+  ionViewWillEnter(){
+    this.getData();
+  }
+  getData(){
+    this.sqlite.create({
+      name: 'ionicdb.db',
+      location: 'default'
+    }).then(
+      (db: SQLiteObject) =>{
+      db.executeSql('CREATE TABLE IF NOT EXISTS expense (rowid INTEGER PRIMARY KEY, date TEXT, type TEXT, description TEXT, amount INTEGER)')
+        .then(res=>console.log('Executed SQL'))
+        .catch(e=>console.log(e));
+      db.executeSql('SELECT * FROM expense ORDER BY rowid DESC')
+      .then(res=>{
+        this.expenses=[];
+        for(var i=0; i<res.rows.length; i++){
+          //อ่านค่าทุกแถวมาใส่ใน object
+          this.expenses.push({
+              rowid: res.rows.item(i).rowid,
+              data: res.rows.item(i).date,
+              type: res.rows.item(i).type,
+              description: res.rows.item(i).description,
+              amount: res.rows.item(i).amount
+          })
+        }
+      })
+      .catch(e=>console.log(e));
+      //หารายรับรวม
+      db.executeSql('SELECT SUM(amount) AS totalIncome FROM expense WHERE type="Income"')
+      .then(res=>{
+        if(res.row.length>0){
+          this.totalIncome = parseInt(res.rows.item(0).totalIncome);
+          this.balance = this.totalIncome-this.totalExpense;
+        }
+      })
+      .catch(e=>console.log(e));
+      //หารายจ่ายรวม
+      db.executeSql('SELECT SUM(amount) AS totalExpense FROM expense WHERE type="Expense"')
+      .then(res=>{
+        if(res.row.length>0){
+          this.totalExpense = parseInt(res.rows.item(0).totalExpense);
+          this.balance = this.totalIncome-this.totalExpense;
+      }
+    })
+  }).catch(e=>console.log(e));
+  }
+  addData(){
+    this.navCtrl.push(AdddataPage);
+  }
+
+  editData(rowid){
+  this.navCtrl.push(EditdataPage, {
+    rowid:rowid
+  });
+  }
+
+  deleteData(rowid){
+    this.sqlite.create({
+      name: 'ionicdb.db',
+      location: 'dedault'
+    })
+    .then(
+      (db: SQLiteObject) =>{
+      db.executeSql('DELETE FROM expense WHERE rowid=?',[rowid])
+        .then(res=>{
+          console.log(res);
+          this.getData();
+        })
+        .catch(e=>console.log(e));
+    })
+    .catch(e=>console.log(e));
+    }
+  }
+
