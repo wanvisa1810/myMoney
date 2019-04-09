8c06a75ccf2cbf240f6363db2ca82c4b627ed061
Wanvisa Suwannin <wanvisa.nun@gmail.com>
2019-04-09 15:18:02 +0700

    update editdata

--- src/pages/editdata/editdata.ts
+++ src/pages/editdata/editdata.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+import { Toast } from '@ionic-native/toast';
 /**
  * Generated class for the EditdataPage page.
  *
@@ -14,12 +15,84 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'editdata.html',
 })
 export class EditdataPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  data = {rowid:0, date:"", type:"", description:"", amount:0};
+  constructor(public navCtrl: NavController, public navParams: NavParams, private sqlite: SQLite, private toast: Toast) {
+      this.getCurrentData(navParams.get('rowid'));
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EditdataPage');
   }
+  
+  getCurrentData(rowid) {
+    this.sqlite.create({ name: 'ionicdb.db', location: 'default'})
+    .then((db: SQLiteObject) => {
+      db.executeSql('SELECT * FROM expense WHERE rowid=?',[rowid])
+      .then(res=>{
+        if(res.rows.length > 0) {
+          this.data.rowid= res.rows.item(0).rowid;
+          this.data.date= res.rows.item(0).date;
+          this.data.type= res.rows.item(0).type;
+          this.data.description = res.rows.item(0).description;
+          this.data.amount = res.rows.item(0).amount;
+        }
+      }
+      )
+      .catch(e=>{
+        console.log(e);
+        this.toast.show(e,'3000','center').subscribe(
+          toast =>{
+            console.log(toast);
+          }
+        );
+      });
+    }).catch (e=> {
+      console.log(e);
+      this.toast.show(e,'3000','center').subscribe(
+        toast => {
+          console.log(toast);
+        }
+      );
+    });
+  }
 
+  updateData(){
+    this.sqlite.create({ name: 'ionicdb.db', location: 'default'
+    }).then((db: SQLiteObject)=>{
+      db.executeSql('UPDATE enpense SET date=?,type=?,description=?,amount=? WHERE rowid=?',
+      [
+        this.data.date,
+        this.data.type,
+        this.data.description,
+        this.data.amount,
+        this.data.rowid
+      ])
+        .then(res => {
+          console.log(res);
+          this.toast.show('Data update','3000','center').subscribe(
+            toast => {
+              console.log(toast);
+              this.navCtrl.popToRoot();
+            }
+          );
+        })
+        .catch(e=>{
+          console.log(e);
+          this.toast.show(e, '3000', 'center').subscribe(
+            toast => {
+              console.log(toast);
+            }
+          );
+        });
+        }
+        )
+        .catch(e=> {
+          console.log(e);
+          this.toast.show(e, '3000', 'center').subscribe(
+            toast => {
+              console.log(toast);
+            }
+          );
+        });
+  }
 }
